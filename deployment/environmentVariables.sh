f25b09c404345549b58b7b3cc18f03e6cb578599
akaasif <khanasif1@gmail.com>
2023-04-21 12:34:40 +1000

    keda IAM code update

--- deployment/environmentVariables.sh
+++ deployment/environmentVariables.sh
@@ -5,3 +5,6 @@ export CLUSTER_NAME="eks-demo-scale"
 export AWS_REGION="us-west-1"
 export ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
 export TEMPOUT=$(mktemp)
+export namespace=keda
+export service_account=keda-service-account
+export IAM_KEDA_ROLE="keda-role"
--- keda/createkeda.sh
+++ keda/createkeda.sh
@@ -19,10 +19,15 @@ echo "**Installing karpenter**"
 echo "Deploy SQS access policy"
 aws iam create-policy --policy-name keda-sqs-policy --policy-document ./sqsPolicy.yaml
 
-oidc_provider=$(aws eks describe-cluster --name ${CLUSTER_NAME} --region us-west-1 --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
-export namespace=keda
-export service_account=keda-service-account
+#Deploy Dynamo access policy
+# This is needed in context to our sample application, its not a KEDA requirement 
+echo "Deploy Dynamo access policy. !!This is needed in context to our sample application, its not a KEDA requirement!!"
+aws iam create-policy --policy-name keda-sqs-policy --policy-document ./dynamoPolicy.yaml
 
+
+oidc_provider=$(aws eks describe-cluster --name ${CLUSTER_NAME} --region us-west-1 --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
+
+echo "Create a trusted relation in role for STS"
 #Create Role Trusted Relation 
 cat >trust-relationship.json <<EOF
 {
@@ -37,13 +42,39 @@ cat >trust-relationship.json <<EOF
       "Condition": {
         "StringEquals": {
           "${oidc_provider}:aud": "sts.amazonaws.com",
-          "${oidc_provider}:sub": "system:serviceaccount:${namespace}:${service_account}"
+          "${oidc_provider}:sub": [
+            "system:serviceaccount:keda:keda-operator",
+            "system:serviceaccount:${namespace}:${service_account}"
+          ]
         }
       }
     }
   ]
 }
 EOF
+
+# Create role for KedaOperator to access SQS for poling and generate STS for operator to connect with AWS resources
+echo "Create role for KedaOperator to access SQS for poling and generate STS for operator to connect with AWS resources"
+aws iam create-role --role-name ${IAM_KEDA_ROLE}  --assume-role-policy-document file://trust-relationship1.json --description "keda role-description"
+echo "Attach SQS polciy to Keda role"
+aws iam attach-role-policy --role-name ${IAM_KEDA_ROLE} --policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/keda-sqs-policy
+echo "Attach dynamo polciy to Keda role"
+aws iam attach-role-policy --role-name ${IAM_KEDA_ROLE} --policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/keda-dynamo-policy
+
+echo "Keda Role details: ${aws iam list-attached-role-policies --role-name keda-role --output text}"
+
+# Add a new  Kubernetes service account and attach keda-role
+echo "Create a K8s service account and attach role"
+
+cat > <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: ${service_account}
+  namespace: keda-test
+EOF
+
+kubectl annotate serviceaccount -n keda-test keda-service-account eks.amazonaws.com/role-arn=arn:aws:iam::$account_id:role/${IAM_KEDA_ROLE}
 
 echo "=========================="
 echo "KEDA Completed"
