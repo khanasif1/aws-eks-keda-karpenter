8df5cc417226197a33967340ab9b192b0dcbcd89
akaasif <khanasif1@gmail.com>
2023-04-26 21:50:58 +1000

    fixed some script issue with keda

--- deployment/keda/createkeda.sh
+++ deployment/keda/createkeda.sh
@@ -56,14 +56,16 @@ EOF
 
 # Create role for KedaOperator to access SQS for poling and generate STS for operator to connect with AWS resources
 echo "Create role for KedaOperator to access SQS for poling and generate STS for operator to connect with AWS resources"
-aws iam create-role --role-name ${IAM_KEDA_ROLE}  --assume-role-policy-document file://deployment/keda/trust-relationship.json --description "keda role-description"
+
+KEDA_ROLE=$(aws iam create-role --role-name ${IAM_KEDA_ROLE}  --assume-role-policy-document file://deployment/keda/trust-relationship.json --description "keda role-description" --output text)
+echo "KEDA ROLE : ${KEDA_ROLE}"
 echo "Attach SQS polciy to Keda role"
 aws iam attach-role-policy --role-name ${IAM_KEDA_ROLE} --policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/${IAM_KEDA_SQS_POLICY}
 echo "Attach dynamo polciy to Keda role"
 aws iam attach-role-policy --role-name ${IAM_KEDA_ROLE} --policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/${IAM_KEDA_DYNAMO_POLICY}
 
-aws iam list-attached-role-policies --role-name ${IAM_KEDA_ROLE} --output text
-
+ATTCH_POLICY_LIST=$(aws iam list-attached-role-policies --role-name ${IAM_KEDA_ROLE} --output text
+echo "ATTCH_POLICY_LIST : ${ATTCH_POLICY_LIST}"
 # Add a new  Kubernetes service account and attach keda-role
 echo "Create a K8s service account and attach role"
 kubectl create namespace keda-test 
